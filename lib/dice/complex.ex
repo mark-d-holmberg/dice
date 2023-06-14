cdbac6273ee6094600b240a9cedfec26c539e5b8
Mark Holmberg <mark.holmberg@divvypay.com>
2023-06-14 10:32:18 -0600

    Complex - Function clause matching
    * Rather than stupid case statements.

--- lib/dice/complex.ex
+++ lib/dice/complex.ex
@@ -30,95 +30,119 @@ defmodule Dice.Complex do
     with {regex, kind}
          when kind in @valid_regex_atoms <-
            matching?(raw) do
-      case kind do
-        :complex_quantity_multiplier ->
-          # "(1d20*2)d(1d10)"
-          with %{
-                 "first_die" => first_die,
-                 "multiplier" => _multiplier,
-                 "second_die" => second_die
-               } <- Regex.named_captures(regex, raw),
-               {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
-                 Parser.parse(first_die),
-               {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
-                 Parser.parse(second_die) do
-            {:ok,
-             %{
-               builder
-               | quantity_expression: new_quantity_expr,
-                 sides_expression: new_sides_expr
-             }}
-          end
-
-        :complex_multiplier_ends_with ->
-          # "(1d20*2)d20"
-          with %{
-                 "first_die" => first_die,
-                 "multiplier" => _multiplier,
-                 "ends_with" => ends_with
-               } <- Regex.named_captures(regex, raw),
-               {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
-                 Parser.parse(first_die),
-               {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
-                 Parser.parse(ends_with) do
-            {:ok,
-             %{
-               builder
-               | quantity_expression: new_quantity_expr,
-                 sides_expression: new_sides_expr
-             }}
-          end
-
-        :complex ->
-          # "(1d4)d(2d8)"
-          with %{"first_die" => first_die, "second_die" => second_die} <-
-                 Regex.named_captures(regex, raw),
-               {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
-                 Parser.parse(first_die),
-               {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
-                 Parser.parse(second_die) do
-            {:ok,
-             %{
-               builder
-               | quantity_expression: new_quantity_expr,
-                 sides_expression: new_sides_expr
-             }}
-          end
-
-        :starts_with ->
-          # "(1d4)d20"
-          with %{"first_die" => first_die, "ends_with" => ends_with} <-
-                 Regex.named_captures(regex, raw),
-               {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
-                 Parser.parse(first_die),
-               {:ok, %Rollable{expressable: %Expression{sides: new_sides}}} <-
-                 Parser.parse(ends_with) do
-            {:ok,
-             %{
-               builder
-               | quantity_expression: new_quantity_expr,
-                 sides_expression: nil,
-                 sides: new_sides
-             }}
-          end
-
-        :ends_with ->
-          # "6d(2d12)"
-          with %{"quantity" => new_quantity, "second_die" => second_die} <-
-                 Regex.named_captures(regex, raw),
-               {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
-                 Parser.parse(second_die) do
-            {:ok,
-             %{
-               builder
-               | sides_expression: new_sides_expr,
-                 quantity_expression: nil,
-                 quantity: String.to_integer(new_quantity)
-             }}
-          end
-      end
+      regex
+      |> Regex.named_captures(raw)
+      |> handle_regex_named_captures(builder)
     else
       _ -> {:error, "No regex matches the raw string: #{inspect(raw)}"}
     end
+  end
+
+  # "(1d20*2)d(1d10)"
+  # :complex_quantity_multiplier
+  defp handle_regex_named_captures(
+         %{
+           "first_die" => first_die,
+           "multiplier" => _multiplier,
+           "second_die" => second_die
+         },
+         %Builder{} = builder
+       ) do
+    with {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
+           Parser.parse(first_die),
+         {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
+           Parser.parse(second_die) do
+      {:ok,
+       %{
+         builder
+         | quantity_expression: new_quantity_expr,
+           sides_expression: new_sides_expr
+       }}
+    end
+  end
+
+  # "(1d20*2)d20"
+  # :complex_multiplier_ends_with
+  defp handle_regex_named_captures(
+         %{
+           "first_die" => first_die,
+           "multiplier" => _multiplier,
+           "ends_with" => ends_with
+         },
+         %Builder{} = builder
+       ) do
+    with {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
+           Parser.parse(first_die),
+         {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
+           Parser.parse(ends_with) do
+      {:ok,
+       %{
+         builder
+         | quantity_expression: new_quantity_expr,
+           sides_expression: new_sides_expr
+       }}
+    end
+  end
+
+  # "(1d4)d(2d8)"
+  # :complex
+  defp handle_regex_named_captures(
+         %{"first_die" => first_die, "second_die" => second_die},
+         %Builder{} = builder
+       ) do
+    with {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
+           Parser.parse(first_die),
+         {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
+           Parser.parse(second_die) do
+      {:ok,
+       %{
+         builder
+         | quantity_expression: new_quantity_expr,
+           sides_expression: new_sides_expr
+       }}
+    end
+  end
+
+  # "(1d4)d20"
+  # :starts_with
+  defp handle_regex_named_captures(
+         %{"first_die" => first_die, "ends_with" => ends_with},
+         %Builder{} = builder
+       ) do
+    with {:ok, %Rollable{expressable: %Expression{} = new_quantity_expr}} <-
+           Parser.parse(first_die),
+         {:ok, %Rollable{expressable: %Expression{sides: new_sides}}} <-
+           Parser.parse(ends_with) do
+      {:ok,
+       %{
+         builder
+         | quantity_expression: new_quantity_expr,
+           sides_expression: nil,
+           sides: new_sides
+       }}
+    end
+  end
+
+  # "6d(2d12)"
+  # :ends_with
+  defp handle_regex_named_captures(
+         %{"quantity" => new_quantity, "second_die" => second_die},
+         %Builder{} = builder
+       ) do
+    with {:ok, %Rollable{expressable: %Expression{} = new_sides_expr}} <-
+           Parser.parse(second_die) do
+      {:ok,
+       %{
+         builder
+         | sides_expression: new_sides_expr,
+           quantity_expression: nil,
+           quantity: String.to_integer(new_quantity)
+       }}
+    end
+  end
+
+  # WTF?
+  defp handle_regex_named_captures(_other, _builder) do
+    :error
   end
 end
